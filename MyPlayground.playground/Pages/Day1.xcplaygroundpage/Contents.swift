225576bfdb0108e974f44c0fbb416d38085e3a86
Gabriel <gabriel.meira@outlook.com>
2023-12-11 17:13:04 -0300

    Day 1 - Comecei Novamente
    Fiz algumas variáveis (var) e constantes (let).

--- MyPlayground.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -1,6 +1,45 @@
 import Foundation
 import Cocoa
 
-var greeting = "Hello, playground"
+// Por que Swift?
+// Então, você deve aprender Swift porque você pode ganhar muito dinheiro com ele, mas também porque ele faz muitas coisas muito bem. Sem cruft, sem confusão, apenas muito poder na ponta dos dedos. O que não gostar?
 
-//: [Next](@next)
+
+// Sobre este curso
+// É um curso prático, com mais coisas para fazer do que estudar/aprofundar. Ainda terá aprofundamento quando necessário. Frase final: Programar é uma arte: não gaste todo o seu tempo afiando o lápis quando deveria estar desenhando.
+
+// Como acompanhar
+// Há um monte de código mostrado neste livro, e eu realmente quero encorajá-lo a tentar tudo sozinho - digite o código em seu computador, execute-o e veja a saída, em seguida, experimente um pouco para ter certeza de que você o entendeu.
+
+// Como criar variáveis e constantes
+
+// A palavra (var) significa criar uma nova variável
+// O sinal de (=) atripui um valor a nossa variável
+
+// Exemplo Var
+
+var nome = "Gabriel
+nome = "Meira"
+nome = "Água"
+
+// Exemplo Constante (let)
+
+let sobrenome = "Meira" // Constante não são permitido mudanças
+
+// Exemplo de print usando var
+
+var playerName = "Gabriel"
+print(playerName)
+
+playerName = "Naflali"
+print(playerName)
+
+playerName = "Wendy"
+print(playerName)
+
+// Observação constante (let)
+// Se possível sempre utilize constantes (let), o swift vai conseguir otimizar melhor.
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"

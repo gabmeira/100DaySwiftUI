47935abb652db2809adbc8f0f9d94c16c5911a09
Gabriel <gabriel.meira@outlook.com>
2023-12-11 16:38:33 -0300

    Day 0
    Printando Hello Work (Olá Mundo) e escrevendo comentários.

---
+++ MyPlayground.playground/Pages/Day0.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+print("\(greeting)")
+
+// Fiz a aula do dia 0, uma aula motivacional, porém com muita informação estrutural do porque começaremos onde começaremos. Vale a pena sempre rever, principalmente na parte 3 que fala sobre erros comuns.

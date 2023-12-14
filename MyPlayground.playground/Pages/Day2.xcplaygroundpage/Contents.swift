3bc43592c2c515f26a49c04ccfcb5fc090029b55
Gabriel <gabriel.meira@outlook.com>
2023-12-14 10:41:33 -0300

    Day 2 - Checkpoint
    Vi sobre interpolações de String e também resolvi o primeiro Checkpoint.

--- MyPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -21,3 +21,35 @@ print(gameOverTwo)
 
 gameOverTwo.toggle()
 print(gameOverTwo)
+
+// Unindo String
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+print(greeting)
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+
+print(lyric)
+
+let name = "Gabriel"
+let age = 30
+let message = "Olá, meu nome é \(name) e tenho \(age) anos."
+
+print(message)
+
+// da para colocar cálculos dentor das interpolações.
+
+print("5 x 5 é \(5 * 5)")
+
+// Checkpoint - Ponto de Verificação 1
+// Converter temperatures de Celsius para Fahrenheit
+
+let celsius = 30.0
+var convert = (celsius * 9) / 5 + 32
+
+print("A temperatura em Fahrenheit é", convert,"°C")

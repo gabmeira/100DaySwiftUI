c6ebf94cc003b48319b3dada21c2cbdfa4568fd7
Gabriel <gabriel.meira@outlook.com>
2023-12-18 13:12:08 -0300

    Day 5 - If, Else, Else if, Switch
    Vi mais sobre condicionais (condições), sendo if, else, else if e switch e operador terceário

--- MyPlayground.playground/Pages/Day5.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day5.xcplaygroundpage/Contents.swift
@@ -90,3 +90,158 @@ let first = Sizes.small
 let second = Sizes.large
 
 print(first < second)
+
+// Else
+
+let age2 = 16
+
+if age2 >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 { // o && deve ser lido como (e) é um operador lógico
+    print("It's a nice day")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true { // o || deve ser lido como (ou) é um operador lógico
+    print("You can buy the game")
+}
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.scooter
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+if score > 9000 {
+    print("It's over 9000!")
+} else if score == 9000 {
+    print("It's exactly 9000!")
+} else {
+    print("It's not over 9000!")
+}
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack an umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == .snow {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me...")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+// operador ternário | WTF (o que, verdadeiro, falso)
+
+let age3 = 18
+let canVote = age3 >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.light
+
+let background = theme == .dark ? "black" : "white"
+print(background)

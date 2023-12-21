db13b375379d0fdb4b94760e7aa17404b0541342
Gabriel <gabriel.meira@outlook.com>
2023-12-20 22:47:07 -0300

    Day 7 - Funções
    Continuando funções, return valores

--- MyPlayground.playground/Pages/Day7.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -37,3 +37,61 @@ print()
 calculateWages(people: 10)
 calculateWages(people: 20)
 calculateWages(people: 50)
+
+let root = sqrt(169)
+
+print(root)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    let first = string1.sorted()
+    let second = string2.sorted()
+    return first == second
+    
+    // ou
+    //return string1.sorted() == string2.sorted()
+    
+    // ou menor ainda
+    // string1.sorted() == string2.sorted() // Isso só funciona quando só temos uma linha de código na função
+}
+
+print(areLettersIdentical(string1: "abc", string2: "cab"))
+
+func pythagoras(a: Double, b: Double) -> Double {
+    let input = a * a + b * b
+    let root = sqrt(input)
+    return root
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+func pythagorasResumida(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+let resultado = pythagoras(a: 3, b: 4)
+print(resultado)
+
+func greet(name: String) -> String {
+    name == "Taylor Swift" ? "Oh wow!" : "Hello, \(name)"
+}
+
+print(greet(name: "Taylor Swift"))
+
+func isEveryoneAdult(ages: [Int]) -> Bool {
+    for age in ages {
+        if age < 18 {
+            return false
+        }
+    }
+    return true
+}
+
+print(isEveryoneAdult(ages: [20, 20, 16, 24])) // Quando a matriz/array tem algum valor abaixo de 18, ele da como falso, mas se todos os valores forem acima de 18 ele da como verdadeiro. Porque ele sai do laço/for sem resultado e cai no return true direto.

6e8ec20bd9e75766eaa540778f563b1102ef1102
Gabriel <gabriel.meira@outlook.com>
2023-12-23 09:04:15 -0300

    Day 9 - Encerramentos
    Encerramentos tem sua sintaxe um pouco diferente.

---
+++ MyPlayground.playground/Pages/Day9.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+import Foundation
+
+// Encerramentos, Passagem de Funções em Funções
+
+let team = ["Gloria", "Suzanne", "Piper", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam)
+
+let captaiFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+})

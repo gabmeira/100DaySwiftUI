bef9da61614efb5c1dede6a1fc0285d62df38174
Gabriel <gabriel.meira@outlook.com>
2024-01-16 14:51:57 -0300

    Day 13 - Protocols and extensions
    Criando Protocolos e vendo sobre Tipos de retornos opacos (opaque)

---
+++ MyPlayground.playground/Pages/Day13.xcplaygroundpage/Contents.swift
@@ -0,0 +1,113 @@
+import Cocoa
+import Foundation
+
+// 01 Protocolos
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+struct Car: Vehicle {
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km.")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+    
+    let name = "Car"
+    var currentPassengers = 1
+}
+
+struct Bicyble: Vehicle {
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km.")
+    }
+    
+    let name = "Bicycle"
+    var currentPassengers = 1
+}
+
+let bike = Bicyble()
+commute(distance: 50, using: bike)
+
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+getTravelEstimates(using: [car, bike], distance: 150)
+
+protocol Purchaseable {
+    var name: String { get set }
+}
+
+func buy(_ item: Purchaseable) {
+    print("I'm buying \(item.name)")
+}
+
+struct Book: Purchaseable {
+    var name: String
+    var author: String
+}
+
+struct Movie: Purchaseable {
+    var name: String
+    var actors: [String]
+}
+
+struct Car2: Purchaseable {
+    var name: String
+    var manufacturer: String
+}
+
+struct Coffee: Purchaseable {
+    var name: String
+    var strength: Int
+}
+
+//* Exercícios 01
+
+protocol Purchaseable2 {
+    var price: Double { get set }
+    var currency: String { get set }
+}
+
+//* 02 Types return opaque
+
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
+
+print(getRandomNumber() == getRandomNumber())
+
+//* 03 Extensões

1f9591839f53325f194a34862b96fbbfd92f518a
Gabriel <gabriel.meira@outlook.com>
2024-01-18 21:11:06 -0300

    Day 13 - Protocols and Extensions
    Usando Extensions em protocols

--- MyPlayground.playground/Pages/Day13.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day13.xcplaygroundpage/Contents.swift
@@ -200,3 +200,65 @@ extension String {
 }
 
 //* 04 Extensões usando em Protocolo
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isEmpty == false {
+    print("Guest count: \(guests.count)")
+}
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
+let numbers = [4, 8, 15, 16]
+let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers2 = Set([4, 8, 15, 16])
+let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
+
+//* Exercícios 04 Protocols e Extensions
+
+protocol Anime {
+    var availableLanguages: [String] { get set }
+    func watch(in language: String)
+}
+
+extension Anime {
+    func watch(in language: String) {
+        if availableLanguages.contains(language) {
+            print("Now playing in \(language)")
+        } else {
+            print("Unrecognized language.")
+        }
+    }
+}
+
+//* CheckPoint 8 - Ponto de Verificação 8

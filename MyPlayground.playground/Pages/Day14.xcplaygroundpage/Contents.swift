e6da31673c7e0d5a7ab40bdf8a295be23463e075
Gabriel <gabriel.meira@outlook.com>
2024-01-22 22:28:07 -0300

    Day 14 - Optionals
    Encadeamento de opcional

--- MyPlayground.playground/Pages/Day14.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day14.xcplaygroundpage/Contents.swift
@@ -395,3 +395,62 @@ var selectedYear: Int? = nil
 let actualYear = selectedYear ?? 1989
 
 //* 04 Varias Options usando encadeamento
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book2 {
+    let title: String
+    let author: String?
+}
+
+var book2: Book2? = nil
+let author3 = book2?.author?.first?.uppercased() ?? "A"
+print(author)
+
+let names2 = ["Vincent": "van Gogh", "Pablo": "Picasso", "Claude": "Monet"]
+let surnameLetter = names2["Vincent"]?.first?.uppercased() ?? "?"
+
+// Exercicios de Optionals encadeamento
+
+let songs: [String]? = [String]()
+let finaSong = songs?.last?.uppercased()
+
+print()
+
+func albumReleased(in year: Int) -> String? {
+    switch year {
+    case 2006: return "Taylor Swift"
+    case 2008: return "Fearless"
+    case 2010: return "Speak Now"
+    case 2012: return "Red"
+    case 2014: return "1989"
+    case 2017: return "Reputation"
+    default: return nil
+    }
+}
+let album2 = albumReleased(in: 2006)?.uppercased()
+
+print()
+
+let shoppingList = ["eggs", "tomatoes", "grapes"]
+let firstItem = shoppingList.first?.appending(" are on my shopping list")
+
+print()
+
+let capitals = ["Scotland": "Edinburgh", "Wales": "Cardiff"]
+let scottishCapital = capitals["Scotland"]?.uppercased()
+
+print()
+
+let favoriteColors = ["Paul": "Red", "Charlotte": "Pink"]
+let charlotteColor = favoriteColors["Charlotte"]?.lowercased()
+
+print()
+
+let racers = ["Hamilton", "Verstappen", "Vettel"]
+let winnerWasVE = racers.first?.hasPrefix("Ve")
+
+//* 05 Falhas função optionals

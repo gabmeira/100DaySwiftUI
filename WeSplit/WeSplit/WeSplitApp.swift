131e677dd1c782f41aadcd5be08ba2b6de3629c2
Gabriel <gabriel.meira@outlook.com>
2024-02-01 16:45:18 -0300

    Day 16 - Project One
    SwiftUI - Primeiro projeto dos 100 days

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriel Bruno Meira on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
